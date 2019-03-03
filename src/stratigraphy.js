1756fd25ce1dde8bd38fd4345d55c486355d2616
April Johnson <april@April-MB-Pro.local>
2019-03-03 11:34:47 -0700

    use left/right half of screen for hover position

--- src/stratigraphy.js
+++ src/stratigraphy.js
@@ -25,9 +25,10 @@ const showHoverImage = (selector, e) => {
   $(selector).addClass("strat-show-img");
   $(selector).addClass("strat-hover-img-position");
   const rect = e.getBoundingClientRect();
+  const midX = window.innerWidth / 2;
   const position = `
-                left: ${rect.right + 100}px; 
-                top: ${rect.top + 50}px;
+                ${rect.x > midX ? "right" : "left"}: 50vw;
+                top: 50vw;
             `;
   $(selector).attr("style", position);
 };
