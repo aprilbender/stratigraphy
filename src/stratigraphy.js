e0c2681abdbbd337d0ee03dd7dddb36352c735a7
April Johnson <april@April-MB-Pro.local>
2019-03-03 11:10:55 -0700

    add prefix awareness to whackHoverImage

--- src/stratigraphy.js
+++ src/stratigraphy.js
@@ -6,9 +6,10 @@
 // that are independently embedded in various pages so there is far
 // less copypasta.
 
-let whackHoverImage = (e, ident, over) => {
+let whackHoverImage = prefix => (e, ident, over) => {
   console.log("whackHoverImage invoked with arguments:");
   console.log("e:", e);
   console.log("ident:", ident);
   console.log("over", over);
+  console.log("using prefix:", prefix);
 };
